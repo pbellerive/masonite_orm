d14c826ad1ffbc1f0208a52915cc86c453446e28
Joe Mancuso <idmann509@gmail.com>
2024-10-27 14:44:17 -0400

    Refactor SQLiteConnection to use a separate method for creating the connection

--- src/masoniteorm/connections/SQLiteConnection.py
+++ src/masoniteorm/connections/SQLiteConnection.py
@@ -59,20 +59,16 @@ class SQLiteConnection(BaseConnection):
         if self.has_global_connection():
             return self.get_global_connection()
 
-        self._connection = self.create_connection()
+        self._connection = sqlite3.connect(self.database, isolation_level=None)
+        self._connection.create_function("REGEXP", 2, regexp)
+
+        self._connection.row_factory = sqlite3.Row
 
         self.enable_disable_foreign_keys()
 
         self.open = 1
 
         return self
-    
-    def create_connection(self):
-        import sqlite3
-        connection = sqlite3.connect(self.database, isolation_level=None)
-        connection.create_function("REGEXP", 2, regexp)
-        connection.row_factory = sqlite3.Row
-        return connection
 
     @classmethod
     def get_default_query_grammar(cls):
